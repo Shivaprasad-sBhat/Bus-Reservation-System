513b405e5c73d85c0cb6d706975e94a0fb5bc5a9
Avinash Kumar <avinash.kumar93801@gmail.com>
2022-12-14 23:59:17 +0530

    Adding Exception Handlers, Global Exception handler class, Error details class and Bus exception class.

---
+++ BusReservation/src/main/java/com/masai/exception/ErrorDetails.java
@@ -0,0 +1,57 @@
+package com.masai.exception;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+public class ErrorDetails {
+	
+	private LocalDateTime timestamp;
+	
+	private String message;
+	
+	private String details;
+	
+	
+	public ErrorDetails() {
+	}
+
+	public ErrorDetails(LocalDateTime timestamp, String message, String details) {
+		this.timestamp = timestamp;
+		this.message = message;
+		this.details = details;
+	}
+
+	public LocalDateTime getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(LocalDateTime timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getDetails() {
+		return details;
+	}
+
+	public void setDetails(String details) {
+		this.details = details;
+	}
+	
+	
+	
+
+}
