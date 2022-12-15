14b216fc95e83b6fee08daf5a334966522103349
Avinash Kumar <avinash.kumar93801@gmail.com>
2022-12-15 23:33:08 +0530

    Merge branch 'main' of https://github.com/Shivaprasad-sBhat/posh-fact-2211 into day3-fw17_0534

---
+++ BusReservation/src/main/java/com/masai/service/BusServiceImpl.java
@@ -0,0 +1,112 @@
+package com.masai.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.masai.exception.BusException;
+import com.masai.model.Bus;
+import com.masai.repository.BusDao;
+
+@Service
+public class BusServiceImpl implements BusService {
+
+	@Autowired
+	BusDao busDao;
+	
+	
+//	Method to save Bus in database.
+	@Override
+	public Bus addBus(Bus bus) throws BusException {
+
+		Optional<Bus> busFind = busDao.findById(bus.getBusId());
+	
+		if(busFind.isPresent()) {
+			throw new BusException("Cannot add Bus because already a Bus is Present with id: "+bus.getBusId());
+		}else {
+			Bus savedBus = busDao.save(bus);
+			return savedBus;
+		}
+		
+	}
+
+// Method to update Bus
+	@Override
+	public Bus updateBus(Bus bus) throws BusException {
+		
+		Optional<Bus> opt = busDao.findById(bus.getBusId());
+		
+		if(opt.isPresent()) {
+			Bus updatedBus=  busDao.save(bus);
+			return updatedBus;
+		}else {
+			throw new BusException("Bus not found with Id: "+bus.getBusId());
+		}
+	}
+
+//	Method to delete bus by busId
+	@Override
+	public Bus deleteBus(Integer busId) throws BusException {
+
+		Optional<Bus> opt = busDao.findById(busId);
+		
+		if(opt.isPresent()) {
+			Bus bus = opt.get();
+			busDao.delete(bus);
+			return bus;
+		}else {
+			throw new BusException("Canot delete bus,because bus is not found with id: "+busId);
+		}
+		
+	}
+
+//	Method to view bus by busId
+	@Override
+	public Bus viewBus(Integer busId) throws BusException {
+
+		Optional<Bus> opt = busDao.findById(busId);
+		
+		if(opt.isPresent()) {
+			Bus bus = opt.get();
+			return bus;
+		}else {
+			throw new BusException("No bus present with Bus id:: "+busId);
+		}
+		
+	}
+
+//	Method for getting all by its type
+	@Override
+	public List<Bus> viewBusByType(String busType) throws BusException {
+
+		List<Bus> buses = busDao.findByBusType(busType);
+		
+		if(buses.isEmpty()) {
+			throw new BusException("No bus found with type "+busType);
+		}else {
+			return buses;
+		}
+		
+	}
+
+//	Method to view all bus.
+	@Override
+	public List<Bus> viewAllBus() throws BusException {
+
+		List<Bus> buses = busDao.findAll();
+		
+		if(buses.isEmpty()) {
+			throw new BusException("No bus found");
+		}else {
+			return buses;
+		}
+	}
+	
+	
+
+
+	
+	
+}
