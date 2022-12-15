b76b208d36e9c143213c6863e07646a3327cf895
Avinash Kumar <avinash.kumar93801@gmail.com>
2022-12-15 23:50:23 +0530

    Some changes

--- BusReservation/src/main/java/com/masai/controller/BusController.java
+++ BusReservation/src/main/java/com/masai/controller/BusController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -20,7 +21,7 @@ import com.masai.service.BusServiceImpl;
 @RequestMapping("/busservices")
 public class BusController {
 
-	
+	@Autowired
 	BusServiceImpl busServices;
 	
 	
