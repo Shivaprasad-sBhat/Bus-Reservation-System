68581258a4facd7b52386e9d466fd60d48ba6b5e
Shivaprasad-sBhat <bhatshivaprasad98@gmail.com>
2022-12-15 14:43:16 +0530

    mapping

--- BusReservation/src/main/java/com/masai/model/Route.java
+++ BusReservation/src/main/java/com/masai/model/Route.java
@@ -3,11 +3,15 @@ package com.masai.model;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -34,6 +38,9 @@ public class Route {
 	@NotNull(message = "can't set as null")
 	private Integer distance;
 	
+	
+	@JsonIgnore
+	@OneToMany(mappedBy = "routes",cascade = CascadeType.ALL)
 	private List<Bus> bus = new ArrayList<>();
 	
 }
--- BusReservation/src/main/java/com/masai/model/User.java
+++ BusReservation/src/main/java/com/masai/model/User.java
@@ -1,18 +1,22 @@
 package com.masai.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.UniqueConstraint;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -28,37 +32,39 @@ public class User {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer userLoginId;
 	
-	@NotNull(message = "bus name cannot set as null")
-	@NotEmpty(message = "bus name cannot set as empty")
-	@NotBlank(message = "bus name cannot set as blank")
+	@NotNull(message = "user name cannot set as null")
+	@NotEmpty(message = "user name cannot set as empty")
+	@NotBlank(message = "user name cannot set as blank")
 	@Column(unique = true)
 	private String userName;
 	
-	@NotNull(message = "bus name cannot set as null")
-	@NotEmpty(message = "bus name cannot set as empty")
-	@NotBlank(message = "bus name cannot set as blank")
-    @Size(min =  8 , message = "Minimum 8 charcters")
+	@NotNull(message = "password cannot set as null")
+	@NotEmpty(message = "password cannot set as empty")
+	@NotBlank(message = "password cannot set as blank")
+    @Size(min =  8 , message = "password length should be minimum 8 charcters")
 	private String password;
 	
-	@NotNull(message = "bus name cannot set as null")
-	@NotEmpty(message = "bus name cannot set as empty")
-	@NotBlank(message = "bus name cannot set as blank")
+	@NotNull(message = "first name  cannot set as null")
+	@NotEmpty(message = "first name cannot set as empty")
+	@NotBlank(message = "first name cannot set as blank")
 	private String firstName;
 	
-	@NotNull(message = "bus name cannot set as null")
-	@NotEmpty(message = "bus name cannot set as empty")
-	@NotBlank(message = "bus name cannot set as blank")
+	@NotNull(message = "last name cannot set as null")
+	@NotEmpty(message = "last name cannot set as empty")
+	@NotBlank(message = "last name cannot set as blank")
 	private String lastName;
 	
-	@NotNull(message = "bus name cannot set as null")
-	@NotEmpty(message = "bus name cannot set as empty")
-	@NotBlank(message = "bus name cannot set as blank")
+	@NotNull(message = "contact cannot set as null")
+	@NotEmpty(message = "contact cannot set as empty")
+	@NotBlank(message = "contact cannot set as blank")
 	private long contact;
 	
 	@Column(unique = true)
-	@Email(message = "not in mail format")
+	@Email(message = "email format is incorrect")
 	private String email;
 	
-	private Reservation reservation;
+	@JsonIgnore
+	@OneToMany(mappedBy = "user",cascade = CascadeType.ALL)
+	private List<Reservation> reservationList = new ArrayList();
 	
 }
