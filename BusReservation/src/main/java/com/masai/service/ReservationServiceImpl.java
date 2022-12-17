9a4c6a061173fcf1799ac3a69fc9e4d13f716c99
Shivaprasad-sBhat <bhatshivaprasad98@gmail.com>
2022-12-17 18:58:42 +0530

    frontend

--- BusReservation/src/main/java/com/masai/service/ReservationServiceImpl.java
+++ BusReservation/src/main/java/com/masai/service/ReservationServiceImpl.java
@@ -16,6 +16,7 @@ import com.masai.model.Reservation;
 import com.masai.model.User;
 import com.masai.repository.BusDao;
 import com.masai.repository.ReservationDao;
+import com.masai.repository.SessionDao;
 import com.masai.repository.UserDao;
 
 import net.bytebuddy.utility.RandomString;
@@ -34,19 +35,19 @@ public class ReservationServiceImpl implements ReservationService{
 
 	
 	
-//	@Autowired
-//	private SessionDao sDao;
-//	
-//	String key = sDao.findUUid;
-//	
+	@Autowired
+	private SessionDao sDao;
+	
+
 	// Seat reservation
 	@Override
-	public Reservation addReservation(Reservation reservation,Integer busId ) throws ReservationException{
-		
+	public Reservation addReservation(Reservation reservation,Integer busId,String key) throws ReservationException{
+	
+		sDao.findByUuid(key)
 		
 	Bus bus =	bDao.findById(busId).orElseThrow(() -> new ReservationException("Bus not found."));
 	
-	User user = uDao.findById(11).orElseThrow(() ->new ReservationException("User details not found."));
+	User user = sDao.findById(11).orElseThrow(() ->new ReservationException("User details not found."));
 	
 	if(reservation.getReservationDateAndTime().isBefore(LocalDateTime.now()))
 		throw new ReservationException("Plese enter future date");
